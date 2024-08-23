86b96006b5172aa30d2913ba760f8cb68a56b623
kyle_Reeves <kylereeves314@gmail.com>
2024-08-23 14:31:50 +1000

    test

--- src/main/java/org/example/cab302_project/HelloApplication.java
+++ src/main/java/org/example/cab302_project/HelloApplication.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
+//test
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
