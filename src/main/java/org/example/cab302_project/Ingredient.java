516e9c85655aae4a93f5b0f8f65dff57adb60176
Inkwang LEE <inkwang616@gmail.com>
2024-09-18 13:44:29 +1000

    Manage ingredient

--- src/main/java/org/example/cab302_project/Ingredient.java
+++ src/main/java/org/example/cab302_project/Ingredient.java
@@ -1,4 +1,48 @@
 package org.example.cab302_project;
 
 public class Ingredient {
+    private int id;
+    private String ingredientName;
+    private int ingredientCount;
+    private int minThreshold;
+    private boolean quickAccess;
+
+    public Ingredient(String ingredientName, int ingredientCount, int minThreshold) {
+        this.ingredientName = ingredientName;
+        this.ingredientCount = ingredientCount;
+        this.minThreshold = minThreshold;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public  void setId(int id) {
+        this.id = id;
+    }
+    public String getIngredientName() {
+        return ingredientName;
+    }
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+    public int getIngredientCount() {
+        return ingredientCount;
+    }
+    public void setIngredientCount(int ingredientCount) {
+        this.ingredientCount = ingredientCount;
+    }
+    public int getMinThreshold() {
+        return minThreshold;
+    }
+
+    public void setMinThreshold(int minThreshold) {
+        this.minThreshold = minThreshold;
+    }
+
+    public boolean isQuickAccess() {
+        return quickAccess;
+    }
+    public void setQuickAccess(boolean quickAccess) {
+        this.quickAccess = quickAccess;
+    }
 }
--- src/main/java/org/example/cab302_project/IngredientsDAO.java
+++ src/main/java/org/example/cab302_project/IngredientsDAO.java
@@ -1,4 +1,41 @@
 package org.example.cab302_project;
 
-public class IngredientsDAO {
+import java.util.ArrayList;
+import java.util.List;
+
+public class IngredientsDAO implements IIngredientDAO {
+
+    public static final ArrayList<Ingredient> ingredients = new ArrayList<>();
+    private  static int autoIncrementedId = 0;
+
+    public IngredientsDAO() {
+
+    }
+
+    @Override
+    public void addIngredient(Ingredient ingredient) {
+        ingredient.setId(autoIncrementedId);
+        autoIncrementedId++;
+        ingredients.add(ingredient);
+    }
+
+    @Override
+    public void deleteIngredient(Ingredient ingredient) {
+        ingredients.remove(ingredient);
+    }
+
+    @Override
+    public Ingredient getIngredient(int id) {
+        for (Ingredient ingredient : ingredients) {
+            if (ingredient.getId() == id) {
+                return ingredient;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Ingredient> getAllIngredients() {
+        return new ArrayList<>(ingredients);
+    }
 }
